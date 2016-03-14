62ac305d4cabc2b6f0f12babc73af42649d7d0af
icaromarley5 <icaromarley5@gmail.com>
2016-03-14 12:43:51 -0300

    Added files via upload
    posicionavel melhorada (questão de sangramento de peixes e plantas)

--- SimuladorCG/include/Posicionavel.h
+++ SimuladorCG/include/Posicionavel.h
@@ -4,7 +4,7 @@
 //------------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------------------------------------------------
 // usado para posicao e direcao.
-//direcao Ã© +1, 0 ou -1 em cada um dos eixos, nÃ£o podendo ser 0 nos trÃªs
+//direcao é +1, 0 ou -1 em cada um dos eixos, não podendo ser 0 nos três
 typedef struct {
 	int x;
 	int y;
@@ -21,7 +21,7 @@ class Posicionavel //pedra planta e peixe herdam dessa classe.
 		void posicionar ();
         //codigo para gerar uma posicao aleatoria x y z
         //checar em Cubo se a posicao esta ocupada ou nao.
-        //pedra e planta sobrecarregam essa funÃ§Ã£o, pois tem propriedades especiais para ela.
+        //pedra e planta sobrecarregam essa função, pois tem propriedades especiais para ela.
         char tipo; // r rock f fish p plant
 
 	public:
@@ -43,27 +43,27 @@ class Peixe: public Posicionavel
 
 		posicao direcao;
 		int massa;
-        int taxa; //taxa de diminuiÃ§Ã£o
+        int taxa; //taxa de diminuição
 		//funcoes privadas da classe peixe, somente os metodos daqui as utilizam.
-        bool explodir(); // tenta se dividir. chama a funÃ§Ã£o diminuir e aloca varios peixes menores no Cubo
-		void diminuir(int i);// diminui massa em i pontos por fome/mordida. se a massa chegar a 0, chama a funÃ§Ã£o morrer
+        bool explodir(); // tenta se dividir. chama a função diminuir e aloca varios peixes menores no Cubo
+		void diminuir(int i);// diminui massa em i pontos por fome/mordida. se a massa chegar a 0, chama a função morrer
 		void morrer(); //destroi o objeto e limpa sua posicao no Cubo
 
 	public:
-		Peixe(int taxa); //construtor. seta os valores iniciais de massa e direÃ§Ã£o (aleatoria). chama metodo posicionar.
+		Peixe(int taxa); //construtor. seta os valores iniciais de massa e direção (aleatoria). chama metodo posicionar.
         //chama o construtor de posicionavel e passa f
         //seta taxa
 
 
 		void setDirecao(posicao direcao);
-		//set direÃ§Ã£o aleatoria
+		//set direção aleatoria
 
 		posicao getDirecao( );
 
 		int getMassa( );
 
-        int nadar()
-		//avanÃ§a 1 unidade na direÃ§Ã£o.
+        int nadar();
+		//avança 1 unidade na direção.
 		//se encontrar planta morde
 		//se encontrar peixe e for maior, come
 		//se encontrar peixe e for menor, morre
@@ -76,14 +76,19 @@ class Peixe: public Posicionavel
 
 
 
-		bool virar();//muda aleatoriamente sua direÃ§Ã£o. testar para nao ficar de cabeÃ§a para baixo
+		bool virar();//muda aleatoriamente sua direção. testar para nao ficar de cabeça para baixo
 
-		void fome(int i); // testa o tempo i. chama a funÃ§Ã£o privada diminuir
+		void fome(int i); // testa o tempo i. chama a função privada diminuir
 
-		void morder(Posicionavel*);// come planta/peixe e almenta a massa.  checa para ver se nao Ã© uma pedra.
+		void morder(Posicionavel*);// come planta/peixe e almenta a massa.  checa para ver se nao é uma pedra.
 		//chama a funcao privada explodir caso a massa ultrapasse do limite
-		//checar se massa do peixe Ã© maior.
+		//checar se massa do peixe é maior.
 		//chama metodo sangrar do mordido.
+		//chama metodo crescer do peixe de acordo com o retorno de sangrar do mordido
+
+		int sangrar();
+		//essa função chama a função morrer.
+		//retorna toda a massa do peixe
 
 };
 
@@ -95,13 +100,13 @@ class Planta: public Posicionavel
 		int massa;
         int taxa; //taxa de crescimento
 		//funcoes privadas da classe planta, somente os metodos daqui as utilizam.
-		bool explodir(); // tenta se dividir. chama a funÃ§Ã£o diminuir e aloca varios peixes no Cubo
-		void diminuir(int i);// diminui massa em i pontos por fome/mordida. se a massa chegar a 0, chama a funÃ§Ã£o morrer
+		bool explodir(); // tenta se dividir. chama a função diminuir e aloca varios peixes no Cubo
+		void diminuir(int i);// diminui massa em i pontos por fome/mordida. se a massa chegar a 0, chama a função morrer
 		void morrer(); //destroi o objeto e limpa sua posicao no Cubo
 
 
 		void posicionar();
-		//pedra e planta sempre sao alocadas no chÃ£o
+		//pedra e planta sempre sao alocadas no chão
 		//planta nao acumula em cima de outras
 
 
@@ -111,9 +116,10 @@ class Planta: public Posicionavel
         //seta taxa de crescimento
 		int getMassa();
 
-		void crescer(); //aumenta massa e chama a funÃ§Ã£o privada diminuir e explodir caso passe do limite
+		void crescer(); //aumenta massa e chama a função privada diminuir e explodir caso passe do limite
 
-		void sangrar(); //ocorre ao ser mordido. chama a funÃ§Ã£o privada diminuir
+		int sangrar(); //ocorre ao ser mordido. chama a função privada diminuir
+		//retorna a quantidade que "sangrou"
 
 
 };
@@ -124,7 +130,7 @@ class Pedra: public Posicionavel
 	private:
 
 		void posicionar();
-        //pedra e planta sempre sao alocadas no chÃ£o
+        //pedra e planta sempre sao alocadas no chão
         //pedra acumula em cima das outras
         //chama o construtor de posicionavel e passa r
 
