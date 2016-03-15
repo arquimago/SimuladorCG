d63391498213cfa657af7cfa6ae28634abe121d8
icaromarley5 <icaromarley5@gmail.com>
2016-03-15 00:13:30 -0300

    Update Posicionavel.cpp
    alguns metodos de planta

--- Simulador/src/Posicionavel.cpp
+++ Simulador/src/Posicionavel.cpp
@@ -1,21 +1,49 @@
 #include "Posicionavel.h"
 
-/*Posicionavel::Posicionavel()
-{
-    //ctor
-}
-
-Posicionavel::~Posicionavel()
-{
-    //dtor
-}*/
-
 Posicionavel::Posicionavel(char c)
 {
-   tipo= c;//ctor
+   this->tipo= c;//ctor
 }
 
 char Posicionavel::getTipo()
 {
-   return tipo;
+   return this->tipo;
+}
+
+int Planta::sangrar()
+{
+
+    return this->diminuir(75);
+}
+
+int Planta::diminuir(int massaPerdida)
+{
+
+    if (massaPerdida < this->massa)
+    {
+        this->massa = this->massa - massaPerdida;
+        return massaPerdida;
+    }
+    else
+    {
+        this->morrer();
+        return this->massa;
+    }
+}
+
+void Planta::morrer ()
+{
+
+}
+
+bool Planta::explodir()
+{
+
+}
+
+void Planta::crescer()
+{
+    this->massa = this->massa + this->taxa;
+    if (this->massa > 1000)
+        this->explodir();
 }
