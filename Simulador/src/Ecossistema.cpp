27c4b5638b3d7ff228e0a064efacba9cf4d85c72
icaromarley5 <icaromarley5@gmail.com>
2016-03-15 00:13:04 -0300

    Update Ecossistema.cpp

--- Simulador/src/Ecossistema.cpp
+++ Simulador/src/Ecossistema.cpp
@@ -1,14 +1,4 @@
-/*#include "Ecossistema.h"
 
-Ecossistema::Ecossistema()
-{
-    //ctor
-}
-
-Ecossistema::~Ecossistema()
-{
-    //dtor
-}*/
 #include "Ecossistema.h"
 #include "stddef.h"
 #include "stdio.h"
@@ -21,29 +11,43 @@ void Ecossistema::inicializar (int x, int y, int z)
 
     aquario.dimensao = new plano[y];
 
-    for(int i = 0; i < y; i++){
-        aquario.dimensao[i].grid = new unidade*[x];
+    for(int iy = 0; iy < y; iy++){
+        aquario.dimensao[iy].grid = new unidade*[x];
     }
 
-    for(int i = 0; i < y; i++) {
+    for(int iy = 0; iy < y; iy++) {
         for(int j = 0; j < x; j++){
-            aquario.dimensao[i].grid[j] = new unidade[z];
+            aquario.dimensao[iy].grid[j] = new unidade[z];
         }
     }
 
-    for(int i = 0; i < y; i++){
+    for(int iy = 0; iy < y; iy++){
         for(int j = 0; j < x; j++){
             for(int k = 0; k < z; k++){
-                if(i==0||i==y-1||k==0||k==z-1||j==0||j==x-1){
-                    aquario.dimensao[i].grid[j][k].ocupante[0] = new Posicionavel('r');
-                    //printf("i %d j %d k % d \n", i,j,k);
-                    aquario.dimensao[i].grid[j][k].ocupante[1] = NULL;
+                if(iy==0||iy==y-1||k==0||k==z-1||j==0||j==x-1){
+                    Ecossistema::ocupar(j,iy,k,0,new Posicionavel('r'));
+                    Ecossistema::ocupar(j,iy,k,1,NULL);
                 } else {
-                    aquario.dimensao[i].grid[j][k].ocupante[0] = NULL;
-                    aquario.dimensao[i].grid[j][k].ocupante[1] = NULL;
+                    Ecossistema::ocupar(j,iy,k,0,NULL);
+                    Ecossistema::ocupar(j,iy,k,1,NULL);
                 }
             }
         }
     }
 }
 cubo Ecossistema::aquario;
+
+Posicionavel** Ecossistema::identificarOcupantes (int x, int y, int z)
+{
+   Posicionavel** ocupantes = new Posicionavel*[2];
+   ocupantes[0]==aquario.dimensao[y].grid[x][z].ocupante[0];
+   ocupantes[1]==aquario.dimensao[y].grid[x][z].ocupante[0];
+
+   return ocupantes;
+
+}
+
+void Ecossistema::ocupar (int x, int y, int z,int i, Posicionavel* corpo)
+{
+    aquario.dimensao[y].grid[x][z].ocupante[i] = corpo;
+}
