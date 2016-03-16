ea9601d04c69154d6489fe275c48cd8b22b92bc4
icaromarley5 <icaromarley5@gmail.com>
2016-03-16 11:56:33 -0300

    Update Ecossistema.h

--- Simulador/include/Ecossistema.h
+++ Simulador/include/Ecossistema.h
@@ -6,7 +6,8 @@
 // definicoes para montar o cubo
 //unidade minima do cubo.
 typedef struct {
-	 Posicionavel* ocupante[3];//ponteiro de duas posições de posicionavel (caso de peixe+ planta)
+	 Posicionavel* ocupante[3];
+	 //ponteiro de duas posições de posicionavel (caso de peixe+ planta)
 	 // 0 é pedra
 	 // 1 é planta
 	 // 2 é peixe
@@ -14,27 +15,30 @@ typedef struct {
 
 //plano formado pelas unidades minimas
 typedef struct {
-	 unidade** grid; // matriz [x][z] de variaveis local
+	 unidade** grid;
+	 // matriz [x][z] de variaveis local
 } plano;
 
 //cubo formado pelos planos
 typedef struct {
-	  plano* dimensao; //vetor de [y] planos
+	  plano* dimensao;
+	  //vetor de [y] planos
 } cubo;
-
 
 // a ideia dessa classe é armazenar o "mapa" do ecossistema, localizações com ponteiros para os respectivos objetos
 class Ecossistema
 {
-    static cubo aquario;
+    private:
+        static cubo aquario;
 	public:
-
-		static void inicializar (int x, int y, int z); // grid = new unidade [x,z];   dimensao = new plano [y];
+		static void inicializar (int x, int y, int z);
+		// grid = new unidade [x,z];   dimensao = new plano [y];
 		//inicializar tudo como nulo
         // arredores de pedra
-		static Posicionavel** identificarOcupantes (int x, int y, int z);// retorna quem ocupa o local   retorna null se não tem
-
+		static Posicionavel** identificarOcupantes (int x, int y, int z);
+		// retorna quem ocupa o local   retorna null se não tem
         static void ocupar(int x,int y, int z,int i, Posicionavel* corpo);
+        //seta posicionavel no cubo
 };
 
 #endif // ECOSSISTEMA_H
