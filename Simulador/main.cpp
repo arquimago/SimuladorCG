301913bc593827c22ca9a2c3f239022ee8d5b4c1
icaromarley5 <icaromarley5@gmail.com>
2016-03-19 19:26:21 -0300

    Update main.cpp

--- Simulador/main.cpp
+++ Simulador/main.cpp
@@ -11,45 +11,25 @@ int main ()
     int qtdPeixe = 3;
     int qtdPedra = 20;
     int qtdPlanta = 4;
-    int altura = 3;  //y
-    int largura = 6; //z
-    int comprimento = 4;//x
+    int altura = 3;  //y     0  2
+    int largura = 6; //z  0  5
+    int comprimento = 4;//x 0 3
     int taxaCrescimentoPlanta = 15;
     int taxaDiminuicaoPeixe = 10;
-
-    Desenho ringue;
-
-
 
     //inicializar cubo
     //inicializar posicoes de pedras, plantas e peixes
     Ecossistema::inicializar(comprimento,altura,largura);
 
-    /* //IMPRESSAO DO INICIALIZAR
-
-         for(int i = 0; i < altura+2; i++){
-            for(int j = 0; j < comprimento+2; j++){
-                for(int k = 0; k < largura+2; k++){
-                   //printf("X = %d Y = %d Z=%d ", j, i, k);
-                   if(Ecossistema::aquario.dimensao[i].grid[j][k].ocupante[0]!=NULL){
-                        printf("%c ", Ecossistema::aquario.dimensao[i].grid[j][k].ocupante[0]->getTipo());
-                   } else {
-                        printf("w ");
-                       // printf("%d ", j);
-                   }
-                }
-                printf(" Quebrou linhaaaaaaa \n");
-            }
-            printf("Quebra ordinaria\n");
-        }
-
-    */
 
     //posicionar plantas pedras e peixes
     //
     for (int i=0 ; i < qtdPedra ; i++) new Pedra();
     for (int i=0 ; i < qtdPlanta ; i++) new Planta(taxaCrescimentoPlanta);
     for (int i=0 ; i < qtdPeixe ; i++) new Peixe(taxaDiminuicaoPeixe);
+
+
+
 
     //programa rodando
     /*
